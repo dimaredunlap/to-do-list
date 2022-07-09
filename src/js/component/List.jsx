be42907788f06b46ecad9a95fff0ccee5772261d
dimaredunlap <dimaredunlap@gmail.com>
2022-07-09 16:41:17 +0000

    layout completed

---
+++ src/js/component/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ToDo from "./ToDo.jsx";
+
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const List = () => {
+	return (
+		<ToDo />
+	);
+};
+
+export default List;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import Header from "./Header.jsx"
+import List from "./List.jsx"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [ InputValue, setInputValue ] = useState('');
+
+	const [ ToDoList, setToDoList] = useState([]);
+
+	const addItem = () => {
+		const newList = [...ToDoList]
+		newList.push(InputValue)
+		setToDoList(newList)
+	}
+
 	return (
 		<div className="list container">
-			<h1>todos</h1>
-			<div className="card">hello world</div>
+			<Header />
+			<input type="text" onChange={e => setInputValue(e.target.value)} value={InputValue}/>
+			<button onClick={addItem}></button>
+			<List ToDoList = {ToDoList}/>
 		</div>
 	);
 };
