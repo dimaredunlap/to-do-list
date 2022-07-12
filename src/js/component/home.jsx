e68e788d0d39c08af77532cad8b061f8d60a20b9
dimaredunlap <dimaredunlap@gmail.com>
2022-07-12 13:37:13 +0000

    updated styling

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,54 +1,68 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [ToDoList, setToDoList] = useState([
+    "Do Homework",
+    "Do Laundry",
+    "Walk the Dog",
+  ]);
 
-	const [ ToDoList, setToDoList] = useState(["Do Homework", "Do Laundry", "Walk the Dog"]);
+  const addItem = (onKeyDownEvent) => {
+    if (onKeyDownEvent.keyCode === 13) {
+      let newTask = onKeyDownEvent.target.value;
+      const newList = [...ToDoList, newTask];
+      setToDoList(newList);
+      newTask = "";
+    }
+  };
+  const removeItem = (index) => {
+    const removeTask = ToDoList.filter((item, i) => i != index);
+    setToDoList(removeTask);
+  };
+  const [isShown, setIsShown] = useState({
+    state: false,
+    index: 0,
+  });
+  const list = ToDoList.map((item, i) => {
+    return (
+      <div className="repeating" key={i}>
+        <li
+          onMouseEnter={() => setIsShown({ state: true, index: i })}
+          onMouseLeave={() => setIsShown({ state: false, index: 0 })}
+        >
+          {item}{" "}
+          {isShown.state === true && isShown.index === i ? (
+            <button onClick={() => removeItem(i)}>X</button>
+          ) : (
+            ""
+          )}
+        </li>
+      </div>
+    );
+  });
 
-	const addItem = (onKeyDownEvent) => {
-		if (
-			onKeyDownEvent.keyCode === 13
-		) {
-			let newTask = onKeyDownEvent.target.value
-			const newList = [...ToDoList, newTask]
-			setToDoList(newList)
-			newTask = ''
-		}
-		
-	}
-	const removeItem = (index) => {
-		const removeTask = ToDoList.filter((item,i)=> i != index);
-		setToDoList(removeTask);
-	};
-	const list = ToDoList.map((item,i) => {
-		return (
-			<div key={i}>
-				<li>
-					{item} 
-					<button onClick={() => removeItem(i)}>X</button>
-				</li>
-			</div>
-		)
-	})
-
-	return (
-		<div className="list container-fluid card">
-			<h1>ToDos</h1>
-			<input type="text" onKeyDown={addItem} placeholder=" " id="fname" name="fname"/>
-			<div>
-				<ul>
-					{list}
-				</ul>
-			</div>
-			<div>
-				<ul className="counter">{list.length} item left</ul>
-			</div>
-		</div>
-		
-	);
+  return (
+    <div className="list container-fluid card">
+      <h1>ToDos</h1>
+      <input
+        type="text"
+        onKeyDown={addItem}
+        placeholder=" "
+        id="fname"
+        name="fname"
+      />
+      <div>
+        <ul>{list}</ul>
+      </div>
+      <div>
+        <ul className="counter">{list.length} item left</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
